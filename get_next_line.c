b324969cb2ebceddff903b06dda6d606aad569b3
Daniel Da Silva Carvalho <danielda@student.42sp.org.br>
2024-11-20 16:13:30 -0300

    testtt

--- get_next_line.c
+++ get_next_line.c
@@ -6,34 +6,86 @@
 /*   By: danielda <danielda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 13:46:42 by danielda          #+#    #+#             */
-/*   Updated: 2024/11/19 18:30:34 by danielda         ###   ########.fr       */
+/*   Updated: 2024/11/20 16:11:48 by danielda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*read_fd(int fd, char *stash)
 {
+	char	*buffer;
+	int		bytes_read;
 
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	while (!ft_strchr(stash, '\n'))
+	{
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read <= 0)
+		{
+			free(buffer);
+			if (bayts_read == 0)
+				return (stash);
+			return (NULL);
+		}
+	buffer[bytes_read] = '\0';
+	stash = ft_strjoin(stash, buffer);
+		if (!stash)
+			return (NULL);
+		return (stash);
+	}
 }
-get_current_line(char *stash)
+
+int	get_current_line(char *stash)
 {
+	int		i;
+	char	*string;
 
+	if (!stash[i] || !stash[0])
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+	i++;
+	string = ft_substr(stash, 0, i + ft_endl(stash));
+	return (string);
 }
+
 char	*string_to_add(char *stash)
 {
+	char	*string;
+	int		i;
+	int		j;
 
+	i = 0;
+	j = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+		return (free(stash), NULL);
+	string = malloc(sizeof(char) * (ft_strlen(stash) - i));
+	if (!string)
+		return (NULL);
+	while (stash[++i])
+		string[j++] = stash[i];
+	string[j] = '\0';
+	free(stash);
+	return (string);
 }
-int get_next_line(int fd, char **line)
+
+int	get_next_line(int fd, char **line)
 {
-	static char *r
-	char buffer[BUFFER_SIZE + 1]
-	char *temp
-	int bytes_read
+	static char	*r;
+	char		buffer[BUFFER_SIZE + 1];
+	char		*temp;
+	int			bytes_read;
 
 	if (fd < 0 || !line || buffer_size <= 0)
 		return (-1);
 	while (bayts_reads = (read(fd, buffer, buffer_size) < 0))
 		buffer [bayts_read] = '\0';
-	??? = get_current_line(??)
-	??? = *string_to_add(??)
-		return (??);
+	temp = read_fd(params)
+		free(buffer);
+	temp = get_current_line()
+		temp =  string_to_add()
+		return ();
 }
